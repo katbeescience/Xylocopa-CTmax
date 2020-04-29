eea533222a1d72443bfc4f12504f97050c66b02b
katbeescience <mkbusby@email.arizona.edu>
2020-04-29 09:57:19 -0700

    Make "Tube" column.

--- clean_up_ascii.R
+++ clean_up_ascii.R
@@ -41,12 +41,16 @@ clean.up.ascii <- function(filename, notefile){
   # Wherever we have the word "Ramp", we should make that into a column called
   # "Ramp" and put whatever follows it in that column.
   
-  Ramp.vector <- str_extract(tidy.note$Notes, pattern=("amp\\s\\d"))
+  tidy.note$Ramp <- str_extract(tidy.note$Notes, pattern=("amp\\s\\d"))
+  tidy.note$Ramp <- gsub(tidy.note$Ramp, pattern="amp", replacement="Ramp")   
   
-  tidy.note <- tidy.note %>%
-    add_column(Ramp = Ramp.vector)
+  # Tube column is a copy of Notes
+  # Extract ramp info
+  # Extract everything remaining that's not a single digit.
   
-  tidy.note$Ramp <- gsub(tidy.note$Ramp, pattern="amp", replacement="Ramp")
+  tidy.note$Tube <- tidy.note$Notes 
+  tidy.note$Tube <- str_remove(tidy.note$Tube, pattern=("amp\\s\\d"))
+  tidy.note$Tube <- str_remove(tidy.note$Tube, pattern=("\\D*"))
   
   # Next, we only want the relevant columns from the data df:
   # CO2, Oxygen, Aux1, Aux2.
