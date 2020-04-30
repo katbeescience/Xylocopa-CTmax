1dc17361df9452b0de0b35a4c1bc21fad7bdfd81
katbeescience <mkbusby@email.arizona.edu>
2020-04-30 13:40:04 -0700

    Tweak plot appearance and save as output file.

--- ggplot_tidydf.R
+++ ggplot_tidydf.R
@@ -3,23 +3,33 @@
 # mkbusby@email.arizona.edu
 # Script started April 15, 2020
 
-ggplot_tidydf <- function (filename, notefile) {
+ggplot_tidydf <- function (filename) {
 
 # Load pacakges.
   
   library(ggplot2)
-  source("clean_up_ascii.R")
   
   # Clean up the file and get it from clean_up_ascii.
   
-  tidy.df <- clean.up.ascii(filename, notefile) 
+  dir <- "~/Documents/Research/Xylocopa-CTmax/2020"
+  find.file <- file.path(dir, "Data", paste0(filename,".txt"))
+  tidy.df <- read.delim(find.file)
 
 # Now make a plot.
 
-ggplot(data=tidy.df, mapping=aes(x=Aux2)) +
+tidy.plot <- ggplot(data=tidy.df, mapping=aes(x=Aux2)) +
   geom_line(aes(y=CO2_Percent)) +
-  ylim(0,.05)
+  ylim(0,.05) +
+  labs(xlab="FoxBox Temperature (C, Aux2)", ylab="CO2 Percent",
+       title=paste0("Plot of data in file: ", filename)) +
+  facet_wrap(tidy.df$Tube) +
+  theme_classic()
+
+ggsave(path=file.path(dir, "Output"), filename=paste0("plot_",filename,".png"),
+       plot="tidy.plot", width=12, height=12, units="in")
+
+return(tidy.plot)
 
 }
 
-ggplot_tidydf(filename="06-19-2019.txt", notefile="Notes_06-19-2019.txt")
+ggplot_tidydf(filename="tidydf_06-19-2019")
