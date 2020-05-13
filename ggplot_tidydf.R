637906da2196278dbc3715559211607ad0f0f4a6
katbeescience <mkbusby@email.arizona.edu>
2020-05-13 08:40:07 -0700

    Filter by tube.

--- ggplot_tidydf.R
+++ ggplot_tidydf.R
@@ -8,19 +8,22 @@ ggplot_tidydf <- function (filename) {
 # Load pacakges.
   
   library(ggplot2)
+  library(tidyverse)
   
   # Clean up the file and get it from clean_up_ascii.
   
   dir <- "~/Documents/Research/Xylocopa-CTmax/2020"
   find.file <- file.path(dir, "Data", paste0(filename,".txt"))
   tidy.df <- read.delim(find.file)
+  tidy.df <- tidy.df %>%
+    filter(Tube == "2" | Tube == "3" | Tube == "4" | Tube == "5")
 
 # Now make a plot.
 
 tidy.plot <- ggplot(data=tidy.df, mapping=aes(x=Aux2)) +
   geom_line(aes(y=CO2_Percent)) +
-  ylim(0,.05) +
-  labs(xlab="FoxBox Temperature (C, Aux2)", ylab="CO2 Percent",
+  ylim(0,.04) +
+  labs(x="Temperature (C, Aux2)", y="CO2 Percent",
        title=paste0("Plot of data in file: ", filename)) +
   facet_wrap(tidy.df$Tube) +
   theme_classic()
